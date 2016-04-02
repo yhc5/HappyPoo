3ff4b7984651caca8869ae317392ff68229fd9c3
Yossra Hamid <yossraih@gmail.com>
2016-04-02 15:58:03 -0400

    StartedPoopingActivity

---
+++ app/src/main/java/comapplication/example/yoonhyung/happypoo/ImageAdapter.java
@@ -0,0 +1,61 @@
+package comapplication.example.yoonhyung.happypoo;
+
+
+//package com.example.HelloGridView;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+
+/**
+ * Created by yossrahamid on 4/2/16.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.SongPoop1, R.drawable.SongPoop2,
+            R.drawable.SongPoop3, R.drawable.SongPoop4,
+            R.drawable.SongPoop5, R.drawable.SongPoop6,
+            R.drawable.SongPoop7, R.drawable.SongPoop8,
+            R.drawable.SongPoop9
+    };
+}
