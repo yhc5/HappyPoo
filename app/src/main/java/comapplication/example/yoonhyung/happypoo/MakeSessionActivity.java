75d67722c03601bd0503de08bf6b0a01b46b6b0d
Yoon Choi <yhc5@duke.edu>
2016-04-02 23:23:27 -0400

    all user fields work & images are resized

--- app/src/main/java/comapplication/example/yoonhyung/happypoo/MakeSessionActivity.java
+++ app/src/main/java/comapplication/example/yoonhyung/happypoo/MakeSessionActivity.java
@@ -9,13 +9,11 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -32,128 +30,96 @@ public class MakeSessionActivity extends Activity implements OnClickListener {
     private Calendar newCalendar;
     private Spinner spinnerColor;
     private Button submitButton;
+    private Spinner spinnerTexture;
+    private Spinner spinnerAmount;
 
-    static final int DATE_DIALOG_ID = 999;
+    private SpinnerMaker spinnerMaker = new SpinnerMaker();
 
+    private static Integer[] textureImageDB = {R.drawable.texture_hardlumps,
+            R.drawable.texture_smoothsausage, R.drawable.texture_watery, R.drawable.texture_sausagelumpy,
+            R.drawable.texture_softblobs, R.drawable.texture_sausagecracked, R.drawable.texture_fluffy, R.drawable.texture_sticky};
+
+    //edit these names later
+    private static String[] textureNameDB = {"hardlumps", "smooth", "watery", "lumpy", "softblobs", "cracked", "sticky"};
+
+    private static Integer[] colorImageDB = {R.drawable.brown_poop, R.drawable.green_poop, R.drawable.yellow_poop,
+            R.drawable.black_poop, R.drawable.white_poop, R.drawable.red_poop};
+
+    //dont need color names. just use position.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("myTag", "make session starting");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_session);
 
-
+        // Record current time
         newCalendar = Calendar.getInstance();
-
-        // DATE
-        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        dateFormatter = new SimpleDateFormat("MM-dd-yyyy", Locale.US);
 
         //findViewsById stuff
+        findViewsByIds();
+
+        //user input fields
+        setDateField();
+        setTimeField();
+        spinnerMaker.makeImageSpinner(spinnerColor, colorImageDB, this);
+        spinnerMaker.makeImageSpinner(spinnerTexture, textureImageDB, this);
+        spinnerMaker.makeTextSpinner(spinnerAmount, this);
+    }
+
+
+    private void findViewsByIds() {
         dateET = (EditText) findViewById(R.id.et_date);
         dateET.setInputType(InputType.TYPE_NULL);
         dateET.requestFocus();
 
-        setDateField();
-
-
-        // TIME
         timeET = (EditText) findViewById(R.id.et_time);
         timeET.setInputType(InputType.TYPE_NULL);
 
-        setTimeField();
+        spinnerColor = (Spinner) findViewById(R.id.spinner_color);
+        spinnerTexture = (Spinner) findViewById(R.id.spinner_texture);
+        spinnerAmount = (Spinner) findViewById(R.id.spinner_amount);
 
-
-        // COLOR
-        setColorField();
-
-
+        submitButton = (Button) findViewById(R.id.make_session_submit);
+        submitButton.setOnClickListener(this);
     }
+
 
     private void setDateField() {
         dateET.setOnClickListener(this);
-
-        //Calendar newCalendar = Calendar.getInstance();
         datePickerDialog = new DatePickerDialog(this, new OnDateSetListener() {
-
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
                 dateET.setText(dateFormatter.format(newDate.getTime()));
             }
-
         },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
     }
 
+
     private void setTimeField() {
         timeET.setOnClickListener(this);
-
         timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onTimeSet(TimePicker view, int hourOfDay,
-                                  int minute) {
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 timeET.setText(hourOfDay + ":" + minute);
             }
         }, newCalendar.get(Calendar.HOUR_OF_DAY), newCalendar.get(Calendar.MINUTE), false);
     }
 
-    private void setColorField() {
-        spinnerColor = (Spinner) findViewById(R.id.spinner_color);
-//        spinnerColor.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-
-        AdapterView.OnItemSelectedListener colorListener = new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                //for testing:
-                Toast.makeText(parent.getContext(),
-                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                //do nothing? ...or set default value.
-            }
-        };
-
-        spinnerColor.setOnItemSelectedListener(colorListener);
-
-    }
-
-
 
     @Override
     public void onClick(View view) {
-        Log.d("myTag", "ONCLICKKKKKK-------------------");
         if (view == dateET) {
             datePickerDialog.show();
         } else if (view == timeET) {
             timePickerDialog.show();
+        } else if (view == submitButton) {
+            Log.d("myTag", "JSON dateTime: ");
+            Log.d("myTag", "JSON duration: ");
+            Log.d("myTag", "JSON color: ");
+            Log.d("myTag", "JSON texture: ");
+            Log.d("myTag", "JSON amount: ");
         }
     }
-
-
-    public void submitMakeSession() {
-
-        spinnerColor = (Spinner) findViewById(R.id.spinner_color);
-        submitButton = (Button) findViewById(R.id.make_session_submit);
-
-        submitButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-//                Toast.makeText(this,
-//                        "OnClickListener : " +
-//                                "\nSpinner 1 : " + String.valueOf(spinner1.getSelectedItem()) +
-//                                "\nSpinner 2 : " + String.valueOf(spinner2.getSelectedItem()),
-//                        Toast.LENGTH_SHORT).show();
-
-
-            }
-
-        });
-    }
-
-
 }
